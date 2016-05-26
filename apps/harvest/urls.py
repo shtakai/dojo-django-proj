9d15bcc12b5881c4b546d5edc702d0023e5a25c4
Alyson Takai <shtakai@gmail.com>
2016-05-26 00:21:01 -0700

    refined

--- apps/harvest/urls.py
+++ apps/harvest/urls.py
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
-from . import views
+from .views import Index
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', Index.as_view()),
 ]
--- apps/harvest/views.py
+++ apps/harvest/views.py
@@ -1,6 +1,48 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.core.exceptions import ImproperlyConfigured
 
-# Create your views here.
-def index(request):
-    print('index')
-    pass
+
+class ApplicationContext(object):
+
+    @classmethod
+    def context(cls):
+        return {
+            'title': '',
+            'brand': 'harVestHub',
+            'page_title': 'harVestHub',
+            'warnings': [],
+            'infos': [],
+            'rightmenu': [
+                {
+                    'url': '/harvest/login',
+                    'title': 'Login',
+                },
+                {
+                    'url': '/harvest/register',
+                    'title': 'Register',
+                },
+            ],
+        }
+
+
+class ApplicationView(object):
+    template = ''
+    context = None
+    user = None
+
+    def get(self, request):
+        print('get', self.get_template())
+        print('context:', self.context)
+        return render(request, self.get_template(), self.context)
+
+    def get_template(self):
+        if self.template == '':
+            raise ImproperlyConfigured('"Template" not defined.')
+        return self.template
+
+
+class Index(ApplicationView, View):
+    context = ApplicationContext.context()
+    context['title'] = 'Welcome!'
+    template = 'index.html'
