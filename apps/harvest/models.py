bc3f558330b526bddb15a0ade4dd7f3011fbf1d4
Alyson Takai <shtakai@gmail.com>
2016-05-26 13:18:48 -0700

    added user_model

--- apps/harvest/models.py
+++ apps/harvest/models.py
@@ -1,4 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 
+class Member(models.Model):
+    CUSTOMER = 'CU'
+    SUPPLIER = 'SP'
+    DELIVERER = 'DE'
+    USER_TYPE_CHOICE = (
+        (CUSTOMER, 'Customer'),
+        (SUPPLIER, 'Supplier'),
+        (DELIVERER, 'Deliverer'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    department = models.CharField(max_length=100)
+    address1 = models.CharField(max_length=255)
+    address2 = models.CharField(max_length=255)
+    city = models.CharField(max_length=50)
+    province = models.CharField(max_length=50)
+    mobile = models.PositiveIntegerField()
+    zipcode = models.IntegerField()
+    usertype = models.CharField(max_length=2,
+                                choices=USER_TYPE_CHOICE,
+                                default=CUSTOMER)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
